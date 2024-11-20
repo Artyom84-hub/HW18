8981b58581ab9b0ac37c69252af3791ffde682bd
Artyom84-hub <artyomgdkv@gmail.com>
2024-11-20 09:40:01 +0300

    the second commit

---
+++ src/Animals.java
@@ -0,0 +1,43 @@
+public abstract class Animals {
+    protected double weight;
+    protected int age;
+    protected String name;
+
+    public Animals() {}
+
+    public Animals(double weight, int age, String name) {
+        this.weight = weight;
+        this.age = age;
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void walk() {
+        System.out.println("Животное идет по земле.");
+    }
+
+    public abstract void move();
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //todo Реализовать класс Утка и Собака по описанным в ТЗ правилам.
+        Duck duck = new Duck(3.5, 2,"кряка");
+        duck.walk();
+        duck.move();
+
+        Dog dog = new Dog(7, 3, "дик");
+        dog.walk();
+        dog.move();
     }
 }
